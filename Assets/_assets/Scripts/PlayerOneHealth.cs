c0aee1a7dcd83e22420a6f305513f7a2ee9580c6
Minh <qminh2702@gmail.com>
2025-08-11 21:45:14 +0700

    hjkv

---
+++ Assets/_assets/Scripts/PlayerOneHealth.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneHealth : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 1000;
+    int currentHealth;
+
+    public HealthBar leftHealthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        leftHealthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        leftHealthBar.SetHealth(currentHealth);
+
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Nguoi choi chet.");
+        animator.SetBool("IsDead", true);
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+
+        // Tắt các script điều khiển 
+        MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+        foreach (var script in scripts)
+        {
+            if (script != this)
+                script.enabled = false;
+        }
+
+        // Gọi hàm biến mất sau 1.5s (thời gian để animation chết chạy xong)
+        Invoke(nameof(Disappear), 1.0f);
+    }
+
+    void Disappear()
+    {
+        Destroy(gameObject);
+    }
+}
