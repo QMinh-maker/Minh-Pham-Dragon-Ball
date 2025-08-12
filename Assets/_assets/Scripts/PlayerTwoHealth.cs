f0f1e926cf932957b80b546630bc4606da96c097
QMinh-maker <qminh2702@gmail.com>
2025-08-12 21:19:38 +0700

    ádc

--- Assets/_assets/Scripts/PlayerTwoHealth.cs
+++ Assets/_assets/Scripts/PlayerTwoHealth.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragonHealth : MonoBehaviour
+public class PlayerTwoHealth : MonoBehaviour
 {
     public Animator animator;
     public int maxHealth = 1000;
     int currentHealth;
 
-    public HealthBar leftHealthBar;
+    public HealthBar RightHealthBar;
+
+
     void Start()
     {
         currentHealth = maxHealth;
-        leftHealthBar.SetMaxHealth(maxHealth);
+        RightHealthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        leftHealthBar.SetHealth(currentHealth);
+        RightHealthBar.SetHealth(currentHealth);
 
         animator.SetTrigger("Hurt");
 
@@ -30,13 +32,13 @@ public class DragonHealth : MonoBehaviour
 
     void Die()
     {
-        Debug.Log("Nguoi choi chet.");
+        Debug.Log("Ke dich chet.");
         animator.SetBool("IsDead", true);
 
+        GetComponent<Rigidbody2D>().isKinematic = true;
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
-
-        // Tắt các script điều khiển 
+        // Tắt các script điều khiển khác nếu cần
         MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
         foreach (var script in scripts)
         {
@@ -45,12 +47,11 @@ public class DragonHealth : MonoBehaviour
         }
 
         // Gọi hàm biến mất sau 1.5s (thời gian để animation chết chạy xong)
-        Invoke(nameof(Disappear), 1.0f);
+        Invoke(nameof(Disappear), 5.0f);
     }
 
     void Disappear()
     {
         Destroy(gameObject);
-    }      
-    
+    }
 }
