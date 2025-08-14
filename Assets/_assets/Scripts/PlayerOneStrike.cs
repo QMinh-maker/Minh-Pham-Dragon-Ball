09bd1323d2491443dd10a195d6b7a3ba7452a067
QMinh-maker <qminh2702@gmail.com>
2025-08-14 20:59:17 +0700

    asdf

--- Assets/_assets/Scripts/PlayerOneStrike.cs
+++ Assets/_assets/Scripts/PlayerOneStrike.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,12 +11,25 @@ public class PlayerOneStrike : MonoBehaviour
     public int attackDamage = 40;
     public float attackRate = 2f;
     float nextAttackTime = 0f;
+
+
+    public static int noOfClicks = 0;
+    float lastClickedTime = 0;
+    float maxcomboDelay = 1;
+
+     void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   
+
     // Update is called once per frame
     void Update()
     {
         if (Time.time >= nextAttackTime)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetMouseButtonDown(0))
             {
                 Strike();
                 nextAttackTime = Time.time + 1f / attackRate;
@@ -26,7 +39,37 @@ public class PlayerOneStrike : MonoBehaviour
 
     void Strike()
     {
-        animator.SetTrigger("Strike");
+        lastClickedTime = Time.time;
+        noOfClicks++;
+        if (noOfClicks == 1)
+        {
+            animator.SetBool("Strike1", true);
+        }
+        noOfClicks = Mathf.Clamp(noOfClicks,0,3);
+
+        if(noOfClicks >=2 && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
+            && animator.GetCurrentAnimatorStateInfo(0).IsName("Strike"))
+        {
+            animator.SetBool("Strike1", false);
+            animator.SetBool("Strike2", true);
+        }
+
+        if (noOfClicks >= 3 && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
+            && animator.GetCurrentAnimatorStateInfo(0).IsName("Strike2"))
+        {
+            animator.SetBool("Strike2", false);
+            animator.SetBool("Strike3", true);
+        }
+
+        if (noOfClicks >= 4 && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
+            && animator.GetCurrentAnimatorStateInfo(0).IsName("Strike3"))
+        {
+            animator.SetBool("Strike3", false);
+            animator.SetBool("Strike4", true);
+        }
+
+
+
 
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position,
             attackRange, enemyLayers);
