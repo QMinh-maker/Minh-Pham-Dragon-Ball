702de38626df06e57a72971f416bcf7e809b75c1
Minh <qminh2702@gmail.com>
2025-08-16 12:58:50 +0700

    gdfz

--- Assets/_assets/Scripts/PlayerOneStrike.cs
+++ Assets/_assets/Scripts/PlayerOneStrike.cs
@@ -9,7 +9,7 @@ public class PlayerOneStrike : MonoBehaviour
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
     public int attackDamage = 40;
-    //public float attackRate = 2f;
+    public float attackRate = 2f;
     float nextAttackTime = 0f;
 
 
@@ -27,41 +27,46 @@ public class PlayerOneStrike : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.time >= nextAttackTime)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Strike();
-                Debug.Log(noOfClicks);
-                //nextAttackTime = Time.time + 1f / attackRate;
-            }
-        }
+       
 
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f 
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike1")) 
         {
-            anim.SetBool("Strike1", false); 
+            anim.SetBool("Strike1", false);
+            Debug.Log("strike 1 dang chay");
         }
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f 
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike2")) 
         {
-            anim.SetBool("Strike2", false); 
+            anim.SetBool("Strike2", false);
+            Debug.Log("strike 2 dang chay");
         }
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f 
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike3")) 
         { 
-            anim.SetBool("Strike3", false); 
-            
+            anim.SetBool("Strike3", false);
+            Debug.Log("strike 3 dang chay");
+
         }
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike4"))
         {
             anim.SetBool("Strike4", false);
+            Debug.Log("strike 4 dang chay");
             noOfClicks = 0;
         }
         if (Time.time - lastClickedTime > maxcomboDelay) 
         { 
             noOfClicks = 0; 
+        }
+
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Strike();                
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
         }
     }
 
@@ -72,28 +77,32 @@ public class PlayerOneStrike : MonoBehaviour
         if (noOfClicks == 1)
         {
             anim.SetBool("Strike1", true);
+            
         }
         noOfClicks = Mathf.Clamp(noOfClicks,0,3);
 
-        if(noOfClicks >=2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
+        if(noOfClicks ==2 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike"))
         {
             anim.SetBool("Strike1", false);
             anim.SetBool("Strike2", true);
+            
         }
 
-        if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
+        if (noOfClicks == 3 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike2"))
         {
             anim.SetBool("Strike2", false);
             anim.SetBool("Strike3", true);
+            
         }
 
-        if (noOfClicks >= 4 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
+        if (noOfClicks == 4 && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f
             && anim.GetCurrentAnimatorStateInfo(0).IsName("Strike3"))
         {
             anim.SetBool("Strike3", false);
             anim.SetBool("Strike4", true);
+            
         }
 
 
